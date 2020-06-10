f184186699ae34e2064e97d611ab4c19639ea8d4
Tyler Wilding <tyler.wilding@workiva.com>
2020-06-10 15:45:34 -0400

    resolve default HTTP client issue

--- eva/http/http_source.go
+++ eva/http/http_source.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"github.com/Workiva/eva-client-go/edn"
 	"github.com/Workiva/eva-client-go/eva"
+	"net"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -163,12 +164,67 @@ func (source *httpSourceImpl) formulateUrl(operation string) string {
 		source.BaseSource.Category())
 }
 
+// NewHTTPClient creates a unique HTTP client to be used on a per client basis.
+//
+// There is a known issue in the golang base http client where if a connection to a
+// remote host fails in an unexpected way, if the request set a custom http header, the
+// caller not only blocks, but it leaves in place a mutex preventing any other new
+// connections on the same http client from reaching that same remote host.
+//
+// See https://github.com/golang/go/issues/33006 for more details.
+//
+// This client is designed to not share any of the global resources with go's builtin
+// http.DefaultClient so that clients can be isolated from this shared failure state.
+//
+// By default, this client also sets a Timeout for all connections running through it.
+// If you feel this timeout does not satisfy your needs, feel free to override it using:
+//
+//   client := platform.NewHTTPClient()
+//   client.Timeout = 60 * time.Second
+//
+// Keep in mind that this timeout should be the MAXIMUM timeout for this client.
+// If you would like a lower, per-request timeout, please set request's Context
+// using req.WithContext(ctx): https://golang.org/pkg/net/http/#Request.WithContext
+func newHTTPClient() *http.Client {
+	// borrowed from net/http::DefaultTransport to ensure we don't have shared globals
+	return &http.Client{
+		Transport: newHTTPTransport(),
+
+		// Default timeout for all requests going through this client.
+		Timeout: 60 * time.Second,
+	}
+}
+
+// NewHTTPTransport creates a unique http transport to be used in an http client.
+//
+// The returned transport has all the same settings as the default global
+// http transport, with the exception of MaxIdleConnsPerHost, which is set to 100.
+func newHTTPTransport() *http.Transport {
+	// borrowed from net/http::DefaultTransport to ensure we don't have shared globals
+	dialer := &net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+		DualStack: true,
+	}
+
+	return &http.Transport{
+		Proxy:                 http.ProxyFromEnvironment,
+		DialContext:           dialer.DialContext,
+		ForceAttemptHTTP2:     true, // added in go 1.13, if your build breaks here.. update
+		MaxIdleConns:          100,
+		MaxIdleConnsPerHost:   100, // note: different from default global transport
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+}
+
 // call the uri with the provided form.
 func (source *httpSourceImpl) call(method string, uri string, form url.Values) (result eva.Result, err error) {
 
 	if source.callClient != nil {
 		var req *http.Request
-		client := &http.Client{}
+		client := newHTTPClient()
 
 		var serializer edn.Serializer
 		if serializer, err = source.Serializer(); err == nil {
